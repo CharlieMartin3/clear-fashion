830f86bc590f360c18d5d1f25912a310b86d7468
CharlieMartin3 <charlie_martin@orange.fr>
2022-03-08 20:01:11 +0100

    TD3

--- server/sandbox.js
+++ server/sandbox.js
@@ -6,18 +6,36 @@ const adressebrand = require('./sources/adressebrand');
 //https://www.montlimart.com/toute-la-collection.html
 //https://www.dedicatedbrand.com/en/men/news
 //https://adresse.paris/630-toute-la-collection
+//'https://adresse.paris/630-toute-la-collection'
 
-async function sandbox (eshop = 'https://adresse.paris/630-toute-la-collection') {
+async function sandbox (eshop) {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
+    if(eshop === "https://adresse.paris/630-toute-la-collection"){
+      const products = await adressebrand.scrape(eshop);
+      console.log(products);
+      console.log('done');
+      process.exit(0);
+    }
+    else if(eshop === "https://www.dedicatedbrand.com/en/men/news"){
+      const products = await dedicatedbrand.scrape(eshop);
+      console.log(products);
+      console.log('done');
+      process.exit(0);
+    }
+    else{
+      const products = await montlimartbrand.scrape(eshop);
+      console.log(products);
+      console.log('done');
+      process.exit(0);
+    }
+
     //const products = await dedicatedbrand.scrape(eshop);
     //const products = await montlimartbrand.scrape(eshop);
-    const products = await adressebrand.scrape(eshop);
+    
 
-    console.log(products);
-    console.log('done');
-    process.exit(0);
+    
   } catch (e) {
     console.error(e);
     process.exit(1);
