a7bc9cea6df5c0adb643e450ddde136bacb55371
CharlieMartin3 <charlie_martin@orange.fr>
2022-03-08 19:46:46 +0100

    adresse Paris

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,14 +1,19 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
 const montlimartbrand = require('./sources/montlimartbrand');
+const adressebrand = require('./sources/adressebrand');
+
 //https://www.montlimart.com/toute-la-collection.html
 //https://www.dedicatedbrand.com/en/men/news
-async function sandbox (eshop = 'https://www.montlimart.com/toute-la-collection.html') {
+//https://adresse.paris/630-toute-la-collection
+
+async function sandbox (eshop = 'https://adresse.paris/630-toute-la-collection') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
-    
+
     //const products = await dedicatedbrand.scrape(eshop);
-    const products = await montlimartbrand.scrape(eshop);
+    //const products = await montlimartbrand.scrape(eshop);
+    const products = await adressebrand.scrape(eshop);
 
     console.log(products);
     console.log('done');
